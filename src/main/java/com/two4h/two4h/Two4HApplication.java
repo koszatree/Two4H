8b6e93c764bf1c9b70fb645e6d254e936a87770a
koszatree <koszatree@gmail.com>
2024-03-19 00:21:02 +0100

    First configuration and tests

---
+++ src/main/java/com/two4h/two4h/Two4HApplication.java
@@ -0,0 +1,13 @@
+package com.two4h.two4h;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Two4HApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Two4HApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/two4h/two4h/Two4HApplicationTests.java
@@ -0,0 +1,13 @@
+package com.two4h.two4h;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Two4HApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
