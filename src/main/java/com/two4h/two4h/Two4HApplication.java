d05a8e796577c1eb090b90f4afb42294ab75f0c9
Mixxu <teemo1709@interia.pl>
2024-04-23 19:32:45 +0200

    Cors error fix

--- src/main/java/com/two4h/two4h/Two4HApplication.java
+++ src/main/java/com/two4h/two4h/Two4HApplication.java
@@ -2,12 +2,30 @@ package com.two4h.two4h;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class Two4HApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Two4HApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Two4HApplication.class, args);
+    }
 
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        String[] allowDomains = new String[2];
+        allowDomains[0] = "http://localhost:4200/";
+        System.out.println("CORS configuration....");
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins(allowDomains)
+                        .allowedMethods("GET","POST","PUT","DELETE");
+            }
+        };
+    }
 }
+
--- src/main/java/com/two4h/two4h/user/UserController.java
+++ src/main/java/com/two4h/two4h/user/UserController.java
@@ -7,7 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@CrossOrigin
+@CrossOrigin(origins = "http://localhost:4200/")
 @RequestMapping("/user")
 public class UserController {
 
