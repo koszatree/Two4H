7a59c9103cc4b0c520bd067ec97668c602720265
koszatree <koszatree@gmail.com>
2024-11-01 22:08:47 +0100

    Added methods from OrderService to OrderController

--- src/main/java/com/two4h/two4h/orders/OrderController.java
+++ src/main/java/com/two4h/two4h/orders/OrderController.java
@@ -1,4 +1,29 @@
 package com.two4h.two4h.orders;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin
+@RequestMapping("/api")
 public class OrderController {
+    @Autowired
+    private OrderService orderService;
+
+    @PostMapping(path = "addOrder")
+    public String addOrder(@RequestBody Order order) {
+        return orderService.addOrder(order);
+    }
+
+    @GetMapping(path = "getAllOrders")
+    public List<OrderDTO> getAllOrders() {
+        return orderService.getAllOrders();
+    }
+
+    @GetMapping(path = "getOrderById")
+    public OrderDTO getOrderById(@RequestParam("id") int id) {
+        return orderService.getOrderById(id);
+    }
 }
